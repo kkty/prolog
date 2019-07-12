e9d5b7e5a421958c3d361a8850e08726b609f542
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-07-12 18:29:21 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,301 @@
+export type Term = Application | Constant | Variable;
+
+// `{Functor,Constant,Variable,Application}name` is only for readability
+// and they not affect behaviours
+
+export class Functor {
+  constructor(public readonly name: string) {}
+  toString(): string { return this.name; }
+}
+
+export class Constant {
+  constructor(public readonly name: string) {}
+  toString(): string { return this.name; }
+}
+
+export class Variable {
+  constructor(public readonly name: string) {}
+  toString(): string { return this.name; }
+}
+
+export class Application {
+  constructor(
+    public readonly functor: Functor,
+    public readonly terms: Term[],
+  ) {}
+
+  toString(): string {
+    return `${this.functor.name}(${this.terms.map(term => term.toString()).join(', ')})`;
+  }
+}
+
+export function listVariables(term: Term): Set<Variable> {
+  const variables = new Set<Variable>();
+
+  if (term instanceof Variable) {
+    variables.add(term);
+  }
+
+  if (term instanceof Application) {
+    term.terms.forEach((term) => {
+      listVariables(term).forEach((variable) => {
+        variables.add(variable);
+      });
+    });
+  }
+
+  return variables;
+}
+
+export class Predicate {
+  constructor(public readonly name: string) {}
+  toString(): string { return this.name; }
+}
+
+export class Fact {
+  constructor(
+    public readonly predicate: Predicate,
+    public readonly terms: Term[],
+  ) {}
+
+  // refresh the variables
+  clone(): Fact {
+    const variables = new Set<Variable>();
+
+    this.terms.forEach((term) => {
+      listVariables(term).forEach((variable) => {
+        variables.add(variable);
+      });
+    });
+
+    const substitutions = Array.from(variables).map((variable) => {
+      return new Substitution(variable, new Variable(variable.name));
+    });
+
+    return new Fact(
+      this.predicate,
+      this.terms.map(term => Substitution.applyAll(term, substitutions)),
+    );
+  }
+}
+
+export class Rule {
+  constructor(
+    public readonly left: { predicate: Predicate, terms: Term[] },
+    public readonly right: { predicate: Predicate, terms: Term[] }[],
+  ) {}
+
+  // refresh the variables
+  clone(): Rule {
+    const variables = new Set<Variable>();
+
+    this.left.terms.forEach((term) => {
+      listVariables(term).forEach((variable) => {
+        variables.add(variable);
+      });
+    });
+
+    this.right.forEach(({ terms }) => {
+      terms.forEach((term) => {
+        listVariables(term).forEach((variable) => {
+          variables.add(variable);
+        });
+      });
+    });
+
+    const substitutions = Array.from(variables).map((variable) => {
+      return new Substitution(variable, new Variable(variable.name));
+    });
+
+    const left = {
+      predicate: this.left.predicate,
+      terms: this.left.terms.map(term => Substitution.applyAll(term, substitutions)),
+    };
+
+    const right = this.right.map(({ predicate, terms }) => ({
+      predicate,
+      terms: terms.map(term => Substitution.applyAll(term, substitutions)),
+    }));
+
+    return new Rule(left, right);
+  }
+}
+
+export class Goal {
+  constructor(
+    public readonly predicate: Predicate,
+    public readonly terms: Term[],
+  ) {}
+
+  toString(): string {
+    return `${this.predicate}(${this.terms.map(i => i.toString()).join(', ')})`;
+  }
+}
+
+export class Substitution {
+  constructor(
+    public readonly variable: Variable,
+    public readonly term: Term,
+  ) {}
+
+  apply(term: Term): Term {
+    if (term instanceof Variable) {
+      if (term === this.variable) return this.apply(this.term);
+      return term;
+    }
+
+    if (term instanceof Constant) return term;
+
+    return new Application(
+      term.functor,
+      term.terms.map(term => this.apply(term)),
+    );
+  }
+
+  toString(): string {
+    return `${this.variable.toString()} -> ${this.term.toString()}`;
+  }
+
+  static applyAll(term: Term, substitutions: Substitution[]): Term {
+    return substitutions.reduce((term, substitution) => substitution.apply(term), term);
+  }
+}
+
+export class Constraint {
+  constructor(
+    public readonly left: Term,
+    public readonly right: Term,
+  ) {}
+
+  toString(): string {
+    return `${this.left.toString()} = ${this.right.toString()}`;
+  }
+
+  // return null on failure
+  static unify(constraints: Constraint[]): Substitution[] | null {
+    if (constraints.length === 0) return [];
+
+    // take the first element
+    const [{ left, right }] = constraints;
+
+    if (left === right) {
+      return Constraint.unify(constraints.slice(1));
+    }
+
+    if (left instanceof Variable || right instanceof Variable) {
+      // this conversion is valid as `left instanceof Variable || right instanceof Variable` holds
+      const substitution = (() => {
+        if (left instanceof Variable) return new Substitution(left, right);
+        if (right instanceof Variable) return new Substitution(right, left);
+      })() as Substitution;
+
+      const substitutions = Constraint.unify(
+        constraints.slice(1)
+          .map(constraint => new Constraint(substitution.apply(constraint.left), substitution.apply(constraint.right))),
+      );
+
+      if (!substitutions) return null;
+
+      return [substitution, ...substitutions];
+    }
+
+    if (left instanceof Constant || right instanceof Constant) {
+      if (left === right) return [];
+      return null;
+    }
+
+    // from here, `left instanceof Application && right instanceof Application` holds
+
+    if (left.functor !== right.functor) return null;
+    if (left.terms.length !== right.terms.length) return null;
+
+    const constraintsNew = [];
+    for (let i = 0; i < left.terms.length; i += 1) {
+      constraintsNew.push(new Constraint(left.terms[i], right.terms[i]));
+    }
+
+    return Constraint.unify([
+      ...constraintsNew,
+      ...constraints.slice(1),
+    ]);
+  }
+}
+
+// `space` is a set of facts and rules
+export class Space {
+  constructor(
+    private readonly facts: Fact[],
+    private readonly rules: Rule[],
+  ) {}
+
+  query(goals: Goal[]): Substitution[][] {
+    type Item = { goals: Goal[], substitutions: Substitution[] };
+
+    const queue: Item[] = [
+      { goals, substitutions: [] },
+    ];
+
+    const ans = [];
+
+    while (queue.length) {
+      // this type conversion is valid as queue.length > 0
+      const { goals, substitutions } = queue.shift() as Item;
+
+      console.error(`goals = [${goals.map(i => i.toString()).join(', ')}], substitutions = [${substitutions.map(i => i.toString()).join(', ')}]`);
+
+      if (goals.length === 0) {
+        ans.push(substitutions);
+      }
+
+      for (const goal of goals) {
+        for (const f of this.facts) {
+          const fact = f.clone();
+
+          if (goal.predicate !== fact.predicate) continue;
+          if (goal.terms.length !== fact.terms.length) continue;
+
+          const constraints = [];
+          for (let i = 0; i < goal.terms.length; i += 1) {
+            constraints.push(new Constraint(Substitution.applyAll(goal.terms[i], substitutions), fact.terms[i]));
+          }
+
+          const substitutionsNew = Constraint.unify(constraints);
+
+          // if unification succeeded, push an item to the queue
+          if (substitutionsNew) {
+            queue.push({
+              // we can drop one goal here
+              goals: goals.filter(i => i !== goal),
+              substitutions: [...substitutions, ...substitutionsNew],
+            });
+          }
+        }
+
+        for (const r of this.rules) {
+          const rule = r.clone();
+
+          if (goal.predicate !== rule.left.predicate) continue;
+          if (goal.terms.length !== rule.left.terms.length) continue;
+
+          const constraints = [];
+          for (let i = 0; i < goal.terms.length; i += 1) {
+            constraints.push(new Constraint(Substitution.applyAll(goal.terms[i], substitutions), rule.left.terms[i]));
+          }
+
+          const substitutionsNew = Constraint.unify(constraints);
+
+          // if unification succeeded, push an item to the queue
+          if (substitutionsNew) {
+            queue.push({
+              // replace one goal with new goals
+              goals: [...goals.filter(i => i !== goal), ...rule.right.map(i => new Goal(i.predicate, i.terms))],
+              substitutions: [...substitutions, ...substitutionsNew],
+            });
+          }
+        }
+      }
+    }
+
+    return ans;
+  }
+}
---
+++ test/index.ts
@@ -0,0 +1,104 @@
+import chai from 'chai';
+import 'mocha';
+import { Functor, Constant, Variable, Application, Constraint, Substitution, Predicate, Fact, Rule, Space, Goal } from '../src';
+
+describe('terms can be stringified correctly', () => {
+  it('case 1', () => {
+    const f = new Functor('f');
+    const g = new Functor('g');
+    const c = new Constant('c');
+    const X = new Variable('X');
+    const Y = new Variable('Y');
+
+    const term = new Application(f, [
+      new Application(g, [X, Y]),
+      new Application(f, [X]),
+      c,
+    ]);
+
+    chai.assert.strictEqual(term.toString(), 'f(g(X, Y), f(X), c)');
+  });
+});
+
+describe('substitutions can be applied correctly', () => {
+  it('case 1', () => {
+    const X = new Variable('X');
+    const Y = new Variable('Y');
+    const f = new Functor('f');
+    const c = new Constant('c');
+    const d = new Constant('d');
+
+    const term = new Application(f, [X, new Application(f, [X, Y])]);
+
+    const substituted = Substitution.applyAll(term, [
+      new Substitution(X, c),
+      new Substitution(Y, d),
+    ]);
+
+    chai.assert.strictEqual(substituted.toString(), 'f(c, f(c, d))');
+  });
+});
+
+describe('can process queries correctly', () => {
+  it('case 1', () => {
+    const z = new Constant('z');
+    const add = new Predicate('add');
+    const s = new Functor('s');
+
+    // add(z, Y, Y).
+    const facts = [
+      (() => {
+        const Y = new Variable('Y');
+        return new Fact(add, [z, Y, Y]);
+      })(),
+    ];
+
+    // add(s(X), Y, s(Z)) :- add(X, Y, Z).
+    const rules = [
+      (() => {
+        const X = new Variable('X');
+        const Y = new Variable('Y');
+        const Z = new Variable('Z');
+
+        return new Rule(
+          {
+            predicate: add,
+            terms: [new Application(s, [X]), Y, new Application(s, [Z])],
+          },
+          [
+            { predicate: add, terms: [X, Y, Z] },
+          ],
+        );
+      })(),
+    ];
+
+    const space = new Space(facts, rules);
+
+    const X = new Variable('X');
+
+    // add(s(z), s(s(z)), X)
+    const result = space.query([
+      new Goal(add, [
+        new Application(s, [z]),
+        new Application(s, [new Application(s, [z])]),
+        X,
+      ]),
+    ]);
+
+    // there should be one suitable substituion
+    chai.assert.strictEqual(result.length, 1);
+    chai.assert.strictEqual(Substitution.applyAll(X, result[0]).toString(), 's(s(s(z)))');
+
+    space.query([
+      // add(X, s(z), s(s(z)))
+      new Goal(add, [
+        X,
+        new Application(s, [z]),
+        new Application(s, [new Application(s, [z])]),
+      ]),
+    ])
+      .map((substitutions) => {
+        console.log('X', Substitution.applyAll(X, substitutions).toString());
+      });
+  });
+});
